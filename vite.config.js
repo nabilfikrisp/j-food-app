71065cf7502775150d6faa2ffa9f4ddd81529d31
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-05-26 17:17:04 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/swiperInit.js",
+                "resources/js/discussionSidebar.js",
+                "resources/js/dragAndDrop.js",
+                "resources/js/filter.js",
+                "resources/js/menuButton.js",
+                "resources/css/swiper.css",
+            ],
             refresh: true,
         }),
     ],
